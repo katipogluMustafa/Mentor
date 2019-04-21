d4af4961320ff0b3b49989003dc63615b956abab
Yukawa <mustafakatipoglu98@hotmail.com>
2019-04-21 13:36:52 +0300

    User Upload Profile Image

--- app/src/main/java/com/example/mentor/ProfileUserDetailsActivity.java
+++ app/src/main/java/com/example/mentor/ProfileUserDetailsActivity.java
@@ -2,13 +2,16 @@ package com.example.mentor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class ProfileUserDetailsActivity extends AppCompatActivity {
 
+    private final int PICK_IMAGE_REQUEST = 71;
     private ImageView profile_image;
 
     @Override
@@ -20,7 +23,11 @@ public class ProfileUserDetailsActivity extends AppCompatActivity {
 
     public void setup(){
         profile_image = findViewById(R.id.user_profile_image);
-        Picasso.get().load("http://cdn.journaldev.com/wp-content/uploads/2017/01/android-constraint-layout-sdk-tool-install.png").into(profile_image);
+        Picasso.get().load("https://www.gravatar.com/avatar/1162ffcaa98e640c5ff5ac6e3048ff51?s=32&d=identicon&r=PG").resize(208,154).placeholder(R.drawable.test_doctor_1).into(profile_image);
+    }
+
+    public void uploadImage(View v){
+        startActivityForResult( Intent.createChooser(new Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT),"Select Profile Image"), PICK_IMAGE_REQUEST);
     }
 
 }
