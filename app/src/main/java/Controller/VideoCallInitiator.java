80e11ed7eeedf86588d4e0dd92c2541038706ec8
Yukawa <mustafakatipoglu98@hotmail.com>
2019-05-05 09:03:50 +0300

    Video Call Initiator && Receiver

---
+++ app/src/main/java/Controller/VideoCallInitiator.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import android.app.Activity;
+
+public class VideoCallInitiator extends AbstractVideoCall {
+    private String doctorUID;
+
+    public VideoCallInitiator(Activity currentActivity, String apiID) {
+        super(currentActivity, apiID);
+    }
+
+
+}
---
+++ app/src/main/java/Controller/VideoCallReceiver.java
@@ -0,0 +1,9 @@
+package Controller;
+
+import android.app.Activity;
+
+public class VideoCallReceiver extends AbstractVideoCall {
+    public VideoCallReceiver(Activity currentActivity, String apiID) {
+        super(currentActivity, apiID);
+    }
+}
